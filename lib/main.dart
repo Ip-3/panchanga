3e221e1ace56b8be725877e1b18e745e7fc75947
Dheerendra Purohit <dheerendra.purohit@kobil.com>
2022-09-08 01:25:47 +0530

    initial commit 04

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:panchanga/views/home.dart';
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
